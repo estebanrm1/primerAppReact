15e69e31e312efe7a1af0953f3506fe1b6723d86
estebanrm1 <solsel90@gmail.com>
2023-05-27 11:01:59 -0300

    Agregue los botones y su funcionamiento en el contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 const Contador = () => {
+//Aqui creo mi estado o state
+const [numero, setNumero] = useState(0);
+
     return (
         <section className="my-5">
             <h3>Contador</h3>
             <hr/>
-            <h4>0</h4>
-            <button className="btn btn-primary">+1</button>
+            <h4>{numero}</h4>
+            <button className="btn btn-primary" onClick={()=> setNumero(numero + 1) }>+1</button>
+            <button className="btn btn-primary mx-2" onClick={()=> setNumero(numero - 1) }>-1</button>
+            <button className="btn btn-primary" onClick={()=> setNumero(0) }>Reiniciar</button>
         </section>
     );
 };
