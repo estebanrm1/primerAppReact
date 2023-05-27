b8772d1270408238d209648bb959bd27b5119596
estebanrm1 <solsel90@gmail.com>
2023-05-27 09:59:55 -0300

    Agregue el maquetado del componente contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Contador from './components/Contador';
 import Lista from './components/Lista';
 import Titulos from './components/Titulos';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,12 +12,12 @@ function App() {
       {/* Esto es un comentario del html */}
       <section className='container'>
         <Titulos></Titulos>
-        {/* <Titulos/> */}
         <h3>Servicios:</h3>
         <hr/>
         <Lista />
+        <Contador/>
+        {/* <Titulos/> */}
       </section>
-
     </>
   )
 }
